bec958e326b8ab5b17f13dfd06608ecf662434cb
ichsanlana <75149752+ichsanlana@users.noreply.github.com>
2024-01-05 11:19:19 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import accuracy_score
+from sklearn.metrics.pairwise import ManhattanDistance
 import streamlit as st
 import time
 import pickle
